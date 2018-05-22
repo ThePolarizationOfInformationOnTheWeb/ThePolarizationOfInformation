e93f140c108b268e7b9df24397486e3dfcd42633
pamelabilothomas1023 <pthomas4@nd.edu>
2018-05-22 17:02:46 -0400

    Code to generate weight and index lists

---
+++ create_weights.py
@@ -0,0 +1,35 @@
+import csv
+
+lines = []
+group = "Legalization of Abortion"
+with open(group + ".csv", "r") as f:
+    i = 0
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        if (i == 0):
+            i = 1
+        else:
+            lines.append(row)
+
+weightlist = []
+indexlist = []
+for itema in lines:
+    itemaweights = []
+    itemaindex = []
+    b = 0
+    for itemb in lines:
+        if (itema != itemb):
+            weight = float(itema[4]) - float(itemb[4])
+            weight = 2 - abs(weight)
+            weight = weight/2
+            itemaweights.append(weight)
+            itemaindex.append(b)
+        b = b + 1
+    weightlist.append(itemaweights)
+    indexlist.append(itemaindex)
+
+with open(group + "weightlist.txt", "w") as f:
+    f.write(str(weightlist))
+
+with open(group + "indexlist.txt", "w") as f:
+    f.write(str(indexlist))
