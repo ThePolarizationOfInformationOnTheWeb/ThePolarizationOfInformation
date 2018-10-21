4e7bd8b5fea2ef68020bc8d6942c700863e1c6d6
Charles Dickens <charlieadickens@gmail.com>
2018-10-20 21:20:40 -1000

    progress

--- PolarityCalculator.py
+++ PolarityCalculator.py
@@ -3,7 +3,7 @@ from Clusterer import Clusterer
 
 class PolarityCalculator:
 
-    def _init_(self, topic: str):
+    def __init__(self, topic: str):
         self.topic = topic
         self.back_path_clusterer = None
 
@@ -28,27 +28,29 @@ class PolarityCalculator:
             print("PolarityCalculator: Clustering Not Implemented")
             return None
         else:
-            adj = clusterer.get_adj()
+            weighted_adj = clusterer.get_weighted_adj()
             clustering = clusterer.get_clustering()
 
         # generate networkx graph to obtain conductance values of clusterings after each critical time
         nx_graph = nx.Graph()
 
-        for i in range(len(adj)):
-            nx_graph.add_edges_from([(i, j) for j in range(i, len(adj[i])) if adj[i][j] == 1])
+        for i in range(len(weighted_adj)):
+            nx_graph.add_edges_from([(i, j, {'capacity': weighted_adj[i][j]}) for j in range(i, len(weighted_adj[i])) if weighted_adj[i][j] != 0])
 
         node_list = nx_graph.nodes()
+        capacity = nx.get_edge_attributes(nx_graph, 'capacity')
 
         conductance_values = []
 
         for i in clustering:
             # calculate the cutsize of the clustering to the rest of the graph
-            cut_size = len(nx.edge_boundary(nx_graph, i, list(set(node_list) - set(i))))
+            edge_boundary = nx.edge_boundary(nx_graph, i)
+            cut_size = sum([capacity[(a, b)] for a, b in edge_boundary])
 
             # calculate the volume of the community
             volume_i = 0
             for j in i:
-                volume_i += nx_graph.degree(j)
+                volume_i += sum([capacity[]])
 
             # calculate the volume of the rest of the graph
             volume_not_i = 0
@@ -59,28 +61,28 @@ class PolarityCalculator:
 
         return sum(conductance_values) / len(conductance_values)
 
-    # calculate the size range of clusterings
-    # clustering - the list of communities found by the backward path algorithm
-    def cluster_size_range(clustering):
-        low = len(clustering[0])  # smallest cluster size intialization
-        high = low  # greatest cluster size initialization
-
-        for i in clustering:
-            # compare low and high to size of i and update low and high appropriately
-            if low > len(i):
-                low = len(i)
-
-            if high < len(i):
-                high = len(i)
-
-        return high - low
-
-    # calculate the size median of clusterings
-    # clustering - the list of communities found by the backward path algorithm
-    def cluster_size_median(clustering):
-        size_list = []  # list of community sizes
-
-        for i in clustering:
-            size_list.append(len(i))
-
-        return np.median(np.array(size_list))
+    # # calculate the size range of clusterings
+    # # clustering - the list of communities found by the backward path algorithm
+    # def cluster_size_range(clustering):
+    #     low = len(clustering[0])  # smallest cluster size intialization
+    #     high = low  # greatest cluster size initialization
+    #
+    #     for i in clustering:
+    #         # compare low and high to size of i and update low and high appropriately
+    #         if low > len(i):
+    #             low = len(i)
+    #
+    #         if high < len(i):
+    #             high = len(i)
+    #
+    #     return high - low
+    #
+    # # calculate the size median of clusterings
+    # # clustering - the list of communities found by the backward path algorithm
+    # def cluster_size_median(clustering):
+    #     size_list = []  # list of community sizes
+    #
+    #     for i in clustering:
+    #         size_list.append(len(i))
+    #
+    #     return np.median(np.array(size_list))
