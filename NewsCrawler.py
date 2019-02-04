20262981ae257a3843f0c09d0188311f17693bcb
Charles Dickens <charlieadickens@gmail.com>
2019-02-04 13:14:03 -1000

    getting links!

--- NewsCrawler.py
+++ NewsCrawler.py
@@ -1,22 +1,35 @@
-# import scrapy
-#
-# class NewsCrawler(CrawlSpider):
 import scrapy
+from scrapy.spiders import CrawlSpider
+from scrapy.linkextractors import LinkExtractor
 
+class NewsSpider(CrawlSpider):
+    name = 'google_news'
 
-class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    start_urls = [
-        'http://quotes.toscrape.com/tag/humor/',
-    ]
+    def __init__(self, topic: str):
+        super().__init__()
+        assert topic is not None, "topic is None"
+        self.topic = topic
 
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.xpath('span/small/text()').get(),
-            }
+    def start_requests(self):
+        request_url = "https://news.google.com/search?q="
+        topic_words = self.topic.split()
 
-        next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        for i, word in enumerate(topic_words):
+            if i is not len(topic_words) - 1:
+                request_url = request_url + word + "%20"
+            else:
+                request_url = request_url + word + "&hl=en-US&gl=US&ceid=US%3Aen"
+
+        yield scrapy.Request(request_url, self.parse_google_news_home)
+
+    def parse_google_news_home(self, response):
+        articles = response.xpath('//article')
+        print(articles[0].xpath('.//a/@href')[0].get())
+
+    # def parse_item(self, response):
+    #     self.logger.info('Hi, this is an item page! %s', response.url)
+    #     item = scrapy.Item()
+    #     item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+    #     item['name'] = response.xpath('//td[@id="item_name"]/text()').get()
+    #     item['description'] = response.xpath('//td[@id="item_description"]/text()').get()
+    #     return item
