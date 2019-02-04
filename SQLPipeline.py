f6db866114eeab1f1276fc2fe4f9a8a5d46be267
Andrew Millard <akm.scout@gmail.com>
2019-02-04 13:15:15 -1000

    added pipeline layout

---
+++ SQLPipeline.py
@@ -0,0 +1,35 @@
+import pymysql
+
+host = ''
+user = ''
+passwd = ''
+db = ''
+
+conn = pymysql.connect(host=host, user=user, passwd=passwd, db=db)
+cursor = conn.cursor()
+
+class SQLPipeline(object):
+
+    collection_name = 'scrapy_items'
+
+    def __init__(self, mysql_uri, mysql_db):
+        self.mysql_uri = mysql_uri
+        self.mysql_db = mysql_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mysql_uri=crawler.settings.get('MYSQL_URI'),
+            mysql_db=crawler.settings.get('MYSQL_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymysql.client(self.mysql_uri)
+        self.db = self.client[self.mysql_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(dict(item))
+        return item
